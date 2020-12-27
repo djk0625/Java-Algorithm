cc0d704a88868a4fe16ad220807c4a670908a617
djk0625 <djk0625@naver.com>
2020-12-27 14:04:38 +0900

    2020-12-17

---
+++ Chapter02/Chapter02G.java
@@ -0,0 +1,47 @@
+package Chapter02;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+public class Chapter02G {
+	public static final Scanner scanner = new Scanner(System.in);
+
+	public static void bubbleSort(int[] data, int n)
+	{
+		for(int i = 0 ; i < n ; i++)
+		{
+            for(int j = 0; j < n-1-i; j++)  // 시간 복잡도 고려하여, 안한 부분만 탐색 할 수 있게, i값을 빼주었습니다.
+            {
+                if(data[j] > data[j+1])
+                {
+                    int temp = data[j];
+                    data[j] = data[j+1];
+                    data[j+1] = temp;
+                }
+            }
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		int n = scanner.nextInt();
+		int data[] = new int[n];
+		for(int i = 0 ; i < n ; i++)
+		{
+			data[i] = scanner.nextInt();
+		}
+
+		bubbleSort(data, n);
+
+		for(int i = 0 ; i < n ; i++)
+		{
+			if( i > 0 )
+			{
+				System.out.print(" ");
+			}
+			System.out.print(data[i]);
+		}
+	}
+
+}
